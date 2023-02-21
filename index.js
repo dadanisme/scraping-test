41837cd16ab40e4e2f9bd7aad133d2b00d4b739b
Ramdan <dadanisme@upi.edu>
2023-02-21 20:20:35 +0700

    ignore data.json

--- index.js
+++ index.js
@@ -3,41 +3,54 @@ import * as fs from "fs";
 
 const browser = await launchBrowser();
 
-const page = await browser.newPage();
-const url =
-  "https://www.lamudi.co.id/banten/tangerang-selatan/pamulang/buy/?q=pamulang%20estate&fbclid=IwAR0opVl5uGhZKh3-ef06eOzLep5b69DSajn2WK9SWjWHbfVY30OIGBswye4";
-await page.goto(url, { waitUntil: "networkidle" });
+async function scrapeLamudi({ query, detailed = false }) {
+  const page = await browser.newPage();
+  const url = "https://www.lamudi.co.id/buy/" + query;
+  await page.goto(url, { waitUntil: "networkidle" });
 
-const items = page.locator("div.ListingCell-wrapper");
+  const items = page.locator("div.ListingCell-wrapper");
 
-const allItems = await items.all();
+  const allItems = await items.all();
 
-const data = await Promise.all(
-  allItems.map(async (item) => {
-    const title = await item.locator(".ListingCell-KeyInfo-title").innerText(); // prettier-ignore
-    const address = await item.locator(".ListingCell-KeyInfo-address-text").innerText(); // prettier-ignore
-    const description = await item.locator(".ListingCell-shortDescription").innerText(); // prettier-ignore
-    const price = await item.locator(".PriceSection-FirstPrice").innerText();
+  const data = await Promise.all(
+    allItems.map(async (item) => {
+      const title = await item.locator(".ListingCell-KeyInfo-title").innerText(); // prettier-ignore
+      const address = await item.locator(".ListingCell-KeyInfo-address-text").innerText(); // prettier-ignore
+      const description = await item.locator(".ListingCell-shortDescription").innerText(); // prettier-ignore
+      const price = await item.locator(".PriceSection-FirstPrice").innerText();
 
-    const informationContainer = item.locator(".KeyInformation-attribute_v2"); // prettier-ignore
-    const allInformation = await informationContainer.all();
-    const information = await Promise.all(
-      allInformation.map(async (information) => {
-        const label = await information.locator(".KeyInformation-label_v2").innerText(); // prettier-ignore
-        const value = await information.locator(".KeyInformation-value_v2").innerText(); // prettier-ignore
-        return { label, value };
-      })
-    );
+      const informationContainer = item.locator(".KeyInformation-attribute_v2"); // prettier-ignore
+      const allInformation = await informationContainer.all();
+      const information = await Promise.all(
+        allInformation.map(async (information) => {
+          const label = await information.locator(".KeyInformation-label_v2").innerText(); // prettier-ignore
+          const value = await information.locator(".KeyInformation-value_v2").innerText(); // prettier-ignore
+          return { label, value };
+        })
+      );
 
-    // TO DO: implement scraping details scrapeDetails(...)
-    // const details = await scrapeDetails(item);
+      if (!detailed) return { title, address, description, price, information };
 
-    return { title, address, description, price, information };
-  })
-);
+      const link = await item
+        .locator(".ListingCell-MainImage > a")
+        .getAttribute("href");
+      console.log(link);
+
+      const details = await scrapeDetails(link);
+
+      return { title, address, description, price, information, details, link };
+    })
+  );
+
+  return data;
+}
+
+async function scrapeDetails(url) {
+  // TO DO: implement scraping details
+  return null;
+}
+
+const data = await scrapeLamudi({ query: "?q=jakarta", detailed: true });
 
 fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
-
-console.log("done");
-
 process.exit(0);
